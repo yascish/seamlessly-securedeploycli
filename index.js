019bdfb62fe1b7d05a885fb4d929855e86f3fbb2
yascish <gs1barcelox@hotmail.com>
2024-05-13 16:32:57 +0700

    test commit

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
-function canFinish(numCourses, prerequisites) {
-  const graph = new Map();
-  const visited = new Array(numCourses).fill(0);
-  for (const [course, prerequisite] of prerequisites) {
-    if (!graph.has(course)) graph.set(course, []);
-    graph.get(course).push(prerequisite);
-  }
-  for (let i = 0; i < numCourses; i++) {
-    if (!dfs(i)) return false;
-  }
-  return true;
-  function dfs(course) {
-    if (visited[course] === 1) return false;
-    if (visited[course] === -1) return true;
-    visited[course] = 1;
-    if (graph.has(course)) {
-      for (const prerequisite of graph.get(course)) {
-        if (!dfs(prerequisite)) return false;
-      }
-    }
-    visited[course] = -1;
-    return true;
+function mergeTwoLists(l1, l2) {
+  if (!l1) return l2;
+  if (!l2) return l1;
+  if (l1.val < l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = mergeTwoLists(l1, l2.next);
+    return l2;
   }
 }
